7f85716efcd28baaf123e401da107a48467b193a
MDCDEV <me@mdcdev.me>
2023-03-22 10:31:30 -0500

    Fix: Equisde

--- next.config.js
+++ next.config.js
@@ -8,6 +8,5 @@ const withMDX = require("@next/mdx")({
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
   reactStrictMode: true,
-  basePath: "/docs",
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 });
