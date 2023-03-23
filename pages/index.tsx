15c58dffd3c72957f18dcc91a68cab1b39d83366
MDCDEV <me@mdcdev.me>
2023-03-22 22:18:13 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
-
-//Redirect to /intro
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
 	const router = useRouter();
-	
+
 	useEffect(() => {
-		router.push('/intro');
-	}, []);
+		router.push("/intro").catch(console.error).then().catch(console.error);
+	}, [router]);
 
 	return (
-		//Using tailwindcss make a beautiful loading screen
-		<div className="flex flex-col items-center justify-center h-screen w-screen">
-			<div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+		<div className="flex flex-col items-center justify-center w-screen h-screen">
+			<div className="w-32 h-32 border-b-2 border-t-2 border-gray-900 rounded-full animate-spin"></div>
 			<p className="text-2xl font-bold">Redirecting...</p>
 		</div>
 	);
